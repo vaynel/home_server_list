5512a6aa4fdb2134f793448ec9e01c0510cd8896
vaynel <ssp04041@gmail.com>
2025-07-26 05:53:13 +0000

    📦 finally add sentry-self-hosted as real folder content

---
+++ sentry-self-hosted/install/set-up-and-migrate-database.sh
@@ -0,0 +1,27 @@
+echo "${_group}Setting up / migrating database ..."
+
+if [[ -z "${SKIP_SENTRY_MIGRATIONS:-}" ]]; then
+  # Fixes https://github.com/getsentry/self-hosted/issues/2758, where a migration fails due to indexing issue
+  $dc up --wait postgres
+
+  os=$($dc exec postgres cat /etc/os-release | grep 'ID=debian')
+  if [[ -z $os ]]; then
+    echo "Postgres image debian check failed, exiting..."
+    exit 1
+  fi
+
+  if [[ -n "${CI:-}" || "${SKIP_USER_CREATION:-0}" == 1 ]]; then
+    $dcr web upgrade --noinput --create-kafka-topics
+    echo ""
+    echo "Did not prompt for user creation. Run the following command to create one"
+    echo "yourself (recommended):"
+    echo ""
+    echo "  $dc_base run --rm web createuser"
+    echo ""
+  else
+    $dcr web upgrade --create-kafka-topics
+  fi
+else
+  echo "Skipped DB migrations due to SKIP_SENTRY_MIGRATIONS=$SKIP_SENTRY_MIGRATIONS"
+fi
+echo "${_endgroup}"
